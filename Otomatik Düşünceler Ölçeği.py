8778987d92c6ea5d1653640234ac0a65d6c0f0e3
Goncagül Kemancı <106958079+Jeremisi@users.noreply.github.com>
2023-03-10 16:41:11 +0300

    Update Otomatik Düşünceler Ölçeği.py

--- Otomatik Düşünceler Ölçeği.py
+++ Otomatik Düşünceler Ölçeği.py
@@ -1,218 +1,217 @@
-print("Aşağıda kişilerin zaman zaman aklına gelen bazı düşünceler sıralanmıştır. Lütfen her birini okuyarak, bu düşüncelerin SON BİR HAFTA içinde aklınızdan ne kadar sıklıkla geçtiğini işaretleyiz. Lütfen her bir maddeyi dikkatle okuyunuz  ve maddelerin yanındaki uygun sayıyı aşağıdaki şıkları dikkate alarak işaretleyiniz.")
+print("Aşağıda kişilerin zaman zaman aklına gelen bazı düşünceler sıralanmıştır.\nLütfen her birini okuyarak, bu düşüncelerin SON BİR HAFTA içinde aklınızdan ne kadar sıklıkla geçtiğini işaretleyiz.\nLütfen her bir maddeyi dikkatle okuyunuz  ve maddelerin yanındaki uygun sayıyı aşağıdaki şıkları dikkate alarak işaretleyiniz.")
 
-print("Hiç aklımdan geçmedi: 1")
+print("\nHiç aklımdan geçmedi: 1")
 print("Ender olarak aklımdan geçti: 2")
 print("Arada sırada aklımdan geçti: 3")
 print("Sık sık aklımdam geçti: 4")
 print("Hep aklımdan geçti: 5")
 
-
-print("Tüm dünya bana karşıymış gibi geliyor")
+#1
+print("\nTüm dünya bana karşıymış gibi geliyor")
 x1 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x1 not in [1, 2, 3, 4, 5]:
-    print("Lütfen 1 ile 5 arasında bir değer girin")
+    print("\nLütfen 1 ile 5 arasında bir değer girin")
     x1 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hiçbir işe yaramıyorum")
+#2
+print("\nHiçbir işe yaramıyorum")
 x2 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x2 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x2 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Neden hiç başarılı olamıyorum")
+#3
+print("\nNeden hiç başarılı olamıyorum")
 x3 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x3 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x3 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Beni hiç kimse anlamıyor")
+#4
+print("\nBeni hiç kimse anlamıyor")
 x4 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x4 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x4 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Başkalarını düş kırıklığına uğrattığım oldu")
+#5
+print("\nBaşkalarını düş kırıklığına uğrattığım oldu")
 x5 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x5 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x5 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Devam edebileceğimi sanmıyorum")
+#6
+print("\nDevam edebileceğimi sanmıyorum")
 x6 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x6 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x6 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Keşke daha iyi bir insan olsaydım")
+#7
+print("\nKeşke daha iyi bir insan olsaydım")
 x7 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x7 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x7 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Öyle güçsüzüm ki ...")
+#8
+print("\nÖyle güçsüzüm ki ...")
 x8 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x8 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x8 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hayatım istediğim gibi gitmiyor")
+#9
+print("\nHayatım istediğim gibi gitmiyor")
 x9 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x9 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x9 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Kendimi düş kırıklığına uğrattım")
+#10
+print("\nKendimi düş kırıklığına uğrattım")
 x10 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x10 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x10 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Artık hiçbir şeyin tadı kalmadı")
+#11
+print("\nArtık hiçbir şeyin tadı kalmadı")
 x11 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x11 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x11 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Artık dayanamayacağım")
+#12
+print("\nArtık dayanamayacağım")
 x12 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x12 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x12 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Bir türlü harekete geçemiyorum")
+#13
+print("\nBir türlü harekete geçemiyorum")
 x13 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x13 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x13 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Neyim var benim")
+#14
+print("\nNeyim var benim")
 x14 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x14 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x14= int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Keşke başka bir yerde olsaydım")
+#15
+print("\nKeşke başka bir yerde olsaydım")
 x15 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x15 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x15 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hiçbir şeyin iki ucunu bir araya getiremiyorum")
+#16
+print("\nHiçbir şeyin iki ucunu bir araya getiremiyorum")
 x16 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x16 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x16 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Kendimden nefret ediyorum")
+#17
+print("\nKendimden nefret ediyorum")
 x17 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x17 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x17 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Değersiz bir insanım")
+#18
+print("\nDeğersiz bir insanım")
 x18 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x18 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x18 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Keşke birden yok olabilseydim")
+#19
+print("\nKeşke birden yok olabilseydim")
 x19 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x19 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x19 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Ne zorum var benim")
+#20
+print("\nNe zorum var benim")
 x20 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x20 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x20 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hayatta hep kaybetmeye mahkûmum")
+#21
+print("\nHayatta hep kaybetmeye mahkûmum")
 x21 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x21 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x21 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hayatım karmakarışık")
+#22
+print("\nHayatım karmakarışık")
 x22 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x22 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x22 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Başarısızım")
+#23
+print("\nBaşarısızım")
 x23 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x23 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x23 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hiçbir zaman başaramayacağım")
+#24
+print("\nHiçbir zaman başaramayacağım")
 x24 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x24 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x24 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Kendimi çok çaresiz hissediyorum")
+#25
+print("\nKendimi çok çaresiz hissediyorum")
 x25 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x25 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x25 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Bir şeylerin değişmesi gerek")
+#26
+print("\nBir şeylerin değişmesi gerek")
 x26 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x26 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x26 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Bende mutlaka bir bozukluk olmalı")
+#27
+print("\nBende mutlaka bir bozukluk olmalı")
 x27 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x27 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x27 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Geleceğim kasvetli")
+#28
+print("\nGeleceğim kasvetli")
 x28 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x28 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x28 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hiçbir şey için uğraşmaya değmez")
+#29
+print("\nHiçbir şey için uğraşmaya değmez")
 x29 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x29 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x29 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
 
-print("Hiçbir şeyi bitiremiyorum")
+#30
+print("\nHiçbir şeyi bitiremiyorum")
 x30 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
-
 while x30 not in [1, 2, 3, 4, 5]:
     print("Lütfen 1 ile 5 arasında bir değer girin")
     x30 = int(input("Ne kadar sıklıkla aklınızdan geçti: "))
@@ -225,16 +224,18 @@ x = sum(x)
 
 #SONUÇ (RESULT)
 
-print(f"Toplam puanınız {x}.")
+print(f"\nToplam puanınız {x}.")
 
 if 30 > x:
     print("Testiniz geçersiz. Yanıtlarınızı kontrol ediniz.")
 elif 30<= x <90:
-    print("Otomatik düşünceleriniz ortalama puandan daha düşük")
+    print("Otomatik düşünceleriniz kritik puandan daha düşük")
 elif 90 <= x < 150:
-    print("Otomatik düşünceleriniz ortalama puandan daha fazla")
+    print("Otomatik düşünceleriniz kritik puandan daha fazla")
 else:
     print("Testiniz geçersiz. Yanıtlarınızı kontrol ediniz.")
+
+
 
 
 
